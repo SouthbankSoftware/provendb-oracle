23e416fed3c2f5cf3c9b609b6b189cd22214d929
Guy Harrison <guy@southbanksoftware.com>
2021-01-30 17:31:14 +1100

    Windows binaries

--- makeWin.bat
+++ makeWin.bat
@@ -1,4 +1,11 @@
+@echo on
 cp node_modules\oracledb\build\Release\oracledb-5.1.0-win32-x64.node dist
-pkg --out-path dist -t node12-win . 
+
+ pkg --out-path dist -t node12-win . 
+
 cd dist
-"c:\program files\7-zip\7z" a -y  provendb-oracle-windows.zip provendb-oracle.exe oracledb-5.1.0-win32-x64.node
+
+ "c:\program files\7-zip\7z" a -y  provendb-oracle-windows.zip provendb-oracle.exe oracledb-5.1.0-win32-x64.node
+
+del provendb-oracle.exe
+del oracledb-5.1.0-win32-x64.node
