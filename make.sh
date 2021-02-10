b012b176a983c0ed06e171794acc9b4de60fb21e
Guy Harrison <guy@southbanksoftware.com>
2021-02-10 17:20:55 +1100

    New Installers

--- make.sh
+++ make.sh
@@ -1,18 +1,18 @@
-rm dist/provendb-oracle-linux.zip 
+rm dist/provendb-oracle-linux.tar.gz 
 cp node_modules/oracledb/build/Release/oracledb-5.1.0-linux-x64.node dist
 pkg   -t node12-linux -o dist/provendb-oracle-linux . 
 cd dist
 mv provendb-oracle-linux provendb-oracle
 chmod 755 provendb-oracle
-zip provendb-oracle-linux.zip provendb-oracle oracledb-5.1.0-linux-x64.node
+tar zcvf provendb-oracle-linux.tar.gz provendb-oracle oracledb-5.1.0-linux-x64.node
 
 cd ..
-rm dist/provendb-oracle-darwin.zip 
+rm dist/provendb-oracle-darwin.tar.gz 
 cp node_modules/oracledb/build/Release/oracledb-5.1.0-darwin-x64.node  dist
 pkg  -t node12-darwin -o dist/provendb-oracle-darwin . 
 cd dist
 mv provendb-oracle-darwin provendb-oracle
 chmod 755 provendb-oracle
-zip provendb-oracle-mac.zip provendb-oracle oracledb-5.1.0-darwin-x64.node
+tar zcvf provendb-oracle-mac.tar.gz provendb-oracle oracledb-5.1.0-darwin-x64.node
 
 rm provendb-oracle
