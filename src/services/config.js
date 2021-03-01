350e02a854cf05b9041a164023917437b3019926
Guy Harrison <guy@southbanksoftware.com>
2021-03-02 09:40:47 +1100

    Better error handling for invalid token

--- src/services/config.js
+++ src/services/config.js
@@ -67,7 +67,13 @@ module.exports = {
             }
         }
         log.trace(`Config loaded: ${JSON.stringify(config)}`);
+        if ((!('proofable' in config))
+            || (!('token' in config.proofable))
+            || (config.proofable.token === 'YourTokenHere')) {
+            throw new Error('Invalid  API token - see https://provendb.readme.io/docs/signing-up-and-obtaining-an-api-key');
+        }
 
+ß
         return (config);
     },
     // Save current configuration to the configuration file (unimplemented)
