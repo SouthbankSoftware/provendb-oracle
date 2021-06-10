1f84d0e279a22b5c21d5e71fef66f2bba4e87ab0
Guy Harrison <guy@southbanksoftware.com>
2021-06-10 11:13:50 +1000

    Prepopulate key

---
+++ test/manual.sh
@@ -0,0 +1,6 @@
+provendb-oracle install --config=provendb.yaml --createDemoAccount --dbaPassword=myLongPassword23 --dropExisting --oracleConnect=mubuntu.local:1526/orclpdb1 --provendbPassword=myLongPassword23 --provendbUser=provendbTest --dbaUserName=system
+
+./provendb-oracle install --config=provendb.yaml --createDemoAccount --sysPassword=myLongPassword23 --dropExisting --oracleConnect=guy13.local:1526/orclpdb1 --provendbPassword=myLongPassword23 --provendbUser=provendbTest
+
+notepad provendb.yaml
+
